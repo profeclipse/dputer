899b6d7c6aa35be6d6c89847e063571995047d31
profeclipse <davehunt@me.com>
2024-05-11 14:10:10 -0400

    Did some more linting

--- scripts/label2inc.py
+++ scripts/label2inc.py
@@ -2,19 +2,21 @@
 
 import sys
 
-def doConvert(iname,oname):
-    labels = []
-    with open(oname,'w') as outfile:
-        with open(iname,'r') as infile:
-            for line in infile:
-                (junk,addr,name) = line.rstrip('\n').split(' ')
-                name = name.removeprefix('.')
-                if name[0].isupper(): #name.startswith('__') and not name.startswith('@')
-                    addr = addr.removeprefix('00')
-                    labels.append([name,addr])
-            labels.sort(key=lambda value: value[1])
-            for (name,addr) in labels:
-                outfile.write('{:<30} = ${}\n'.format(name,addr))
 
-if __name__ == '__main__':
-    doConvert(sys.argv[1],sys.argv[2])
+def doConvert(iname, oname):
+    labels = []
+    with open(oname, "w") as outfile:
+        with open(iname, "r") as infile:
+            for line in infile:
+                (junk, addr, name) = line.rstrip("\n").split(" ")
+                name = name.removeprefix(".")
+                if name[0].isupper():
+                    addr = addr.removeprefix("00")
+                    labels.append([name, addr])
+            labels.sort(key=lambda value: value[1])
+            for name, addr in labels:
+                outfile.write("{:<30} = ${}\n".format(name, addr))
+
+
+if __name__ == "__main__":
+    doConvert(sys.argv[1], sys.argv[2])
